ea4fe71376b37f170b51c7de56879fd0dd2eee73
OsamaOracle <O.mustafaoracle@yahoo.com>
2014-12-10 20:47:46 +0200

    Revert "Oracle Scripts"
    This reverts commit 0424a9ae33f8ee80c3cad6dcf0fa336fbdf87a5e.

--- DBA Scripts/DB Growth Per Month last Year.sql
+++
@@ -1,6 +0,0 @@
-select to_char(creation_time, 'RRRR Month') "Month",
-       sum(bytes)/1024/1024 "Growth in Meg"
-  from sys.v_$datafile
- where creation_time > SYSDATE-365
- group by to_char(creation_time, 'RRRR Month')
-/
